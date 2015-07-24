d406abbc3e32c84ce572cc2d8f0a1846fbfe3867
Yasmine <shroomz.ftw@gmail.com>
2015-07-24 20:38:41 +0400

    Testing..

---
+++ server.py
@@ -0,0 +1,45 @@
+import string
+import json
+import os
+import sys
+import webapp2
+
+class InstantAPIServer(webapp2.RequestHandler):
+    def get(self):
+
+        #self.send_response(200)
+        self.response.headers['Content-Type'] = 'application/json'
+
+        file_arg = "source.json"
+
+        try:
+            content = json.loads(open(file_arg).read())
+
+            path = self.request.path[1:]
+            components = string.split(path, '/')
+            print components
+
+            node = content
+            for component in components:
+                if len(component) == 0 or component == "favicon.ico":
+                    continue
+
+                if type(node) == dict:
+                    node = node[component]
+
+                elif type(node) == list:
+                    node = node[int(component)]
+
+        except IOError:
+            self.response.set_status(404)
+            node = json.loads("{\"error\":\"Couldn't find source\"}")
+
+
+        # self.response.write(json.dumps(node,sort_keys=True, indent=4, separators=(',', ': ')))
+
+        self.response.write(json.dumps(node,indent=4))
+
+        return
+
+app = webapp2.WSGIApplication([(r'/.*', InstantAPIServer)],
+debug=os.environ['SERVER_SOFTWARE'].startswith('Dev'))
